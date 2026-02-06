4628005b24a0e5aa177d589dfc5eedccaa112ae7
Maniinotmoney <maniiiiikant@gmail.com>
2026-02-06 16:07:21 +0530

    Delete studio directory

--- studio/schemas/index.js
+++
@@ -1,9 +0,0 @@
-import heroCarousel from './heroCarousel'
-import hero from './hero'
-import about from './about'
-import services from './services'
-import testimonials from './testimonials'
-import contact from './contact'
-import blog from './blog'
-
-export const schemaTypes = [heroCarousel, hero, about, services, testimonials, contact, blog]
