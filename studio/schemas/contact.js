66746aff9c1c2bcf60a9c95739e523b923ea25d1
Maniinotmoney <maniiiiikant@gmail.com>
2026-02-11 12:30:40 +0530

    Add files via upload

---
+++ studio/schemas/contact.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'contact',
+    title: 'Contact Info',
+    type: 'document',
+    fields: [
+        {
+            name: 'email',
+            title: 'Email Address',
+            type: 'string'
+        },
+        {
+            name: 'phone',
+            title: 'Phone Number',
+            type: 'string'
+        },
+        {
+            name: 'address',
+            title: 'Address',
+            type: 'text'
+        }
+    ]
+}
