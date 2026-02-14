c23c8e82cd0802cb01b06873aa472cfbf754f62f
Maniinotmoney <maniiiiikant@gmail.com>
2026-02-14 19:34:01 +0530

    Delete src/studio directory

--- src/studio/sanity.cli.js
+++
@@ -1,9 +0,0 @@
-import { defineCliConfig } from 'sanity/cli'
-
-export default defineCliConfig({
-    api: {
-        projectId: 'czx64h7f',
-        dataset: 'production'
-    },
-    studioHost: 'drinterprises'
-})
